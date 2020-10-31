4bbcb7d24cae582ca80cc46cc47b3336e81dbdf9
Frances Jane Gonzales <francesjanegonzales@Francess-MacBook-Air.local>
2020-10-31 18:55:40 +0800

    added info in modal cards

--- js/script2.js
+++ js/script2.js
@@ -5,120 +5,146 @@ axios.get(API)
         let allData = response.data;
 
         let hydrogen = response.data[0];
-        let carbon = response.data[7];
-        let oxygen = response.data[9];
+        let carbon = response.data[5];
+        let oxygen = response.data[8];
 
-        let nitrogen = response.data[8];
-        let calcium = response.data[21];
-        let phosphorus = response.data[16];
+        let nitrogen = response.data[6];
+        let calcium = response.data[19];
+        let phosphorus = response.data[14];
 
         // let selectedElements = [hydrogen, carbon, oxygen, nitrogen, calcium, phosphorus]
         // console.log(selectedElements)
 
         document.getElementById('hydrogen').innerHTML =
-            `<div class='modal' id='hydrogen'>
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <button type='button' class='close' data-dismiss='modal' aria-label='Close' aria-hidden='true'><span>&times;</span></button>
-                <div class='modal-body'>
-                    <div class='card'>
+            `<div class="modal-dialog modal-lg">
+            <div class="modal-content">
+            <div class='modal-footer'>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class='modal-body' style='margin: 2em;'>
+            <div>
                     <h1 class='card-symbol'>${hydrogen.symbol}</h1>
                     <h3 class='card-name'>${hydrogen.name}</h3>
                     <h5 class='card-groupBlock'>${hydrogen.groupBlock}</h5>
-                    <h5 class='card-facts'>${hydrogen.facts}</h5>
-                    <h5 class='card-yearDiscovered'>${hydrogen.yearDiscovered}</h5>
+                    <h5 class='card-facts'>Facts: ${hydrogen.facts}</h5>
+                    <h5 class='card-yearDiscovered'>Year Discovered: ${hydrogen.yearDiscovered}</h5>
+                    <h5 class='card-history'>History: ${hydrogen.history}</h5>
                     </div>
-                </div>
-                </div>
+                    </div>
             </div>
-        </div>`
+            </div>
+            </div>`
 
+        document.getElementById('carbon').innerHTML =
+            `<div class="modal-dialog modal-lg">
+            <div class="modal-content">
+            <div class='modal-footer'>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class='modal-body' style='margin: 2em;'>
+            <div>
+                    <h1 class='card-symbol'>${carbon.symbol}</h1>
+                    <h3 class='card-name'>${carbon.name}</h3>
+                    <h5 class='card-groupBlock'>${carbon.groupBlock}</h5>
+                    <h5 class='card-facts'>Facts: ${carbon.facts}</h5>
+                    <h5 class='card-yearDiscovered'>Year Discovered: ${carbon.yearDiscovered}</h5>
+                    <h5 class='card-history'>History: ${carbon.history}</h5>
+                    </div>
+                    </div>
+            </div>
+            </div>
+            </div>`
 
+        document.getElementById('oxygen').innerHTML =
+            `<div class="modal-dialog modal-lg">
+            <div class="modal-content">
+            <div class='modal-footer'>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class='modal-body' style='margin: 2em;'>
+            <div>
+                    <h1 class='card-symbol'>${oxygen.symbol}</h1>
+                    <h3 class='card-name'>${oxygen.name}</h3>
+                    <h5 class='card-groupBlock'>${oxygen.groupBlock}</h5>
+                    <h5 class='card-facts'>Facts: ${oxygen.facts}</h5>
+                    <h5 class='card-yearDiscovered'>Year Discovered: ${oxygen.yearDiscovered}</h5>
+                    <h5 class='card-history'>History: ${oxygen.history}</h5>
+                    </div>
+                    </div>
+            </div>
+            </div>
+            </div>`
 
-        //     document.getElementById('hydrogen').innerHTML = `<div class='modal' id='hydrogen'>
-        //     <div class="modal-dialog">
-        //       <div class="modal-content">
-        //         <div class='modal-header'>
-        //           <div class='modal-title'>
-        //             <button type='button' class='close' data-dismiss='modal' aria-label='Close' aria-hidden='true'><span>&times;</span></button>
-        //           </div>
-        //           <div class='modal-body'>
-        //             <div class='card'>
-        //               <h1 class='card-symbol'>${hydrogen.symbol}</h1>
-        //               <h3 class='card-name'>${hydrogen.name}</h3>
-        //               <h5 class='card-groupBlock'>${hydrogen.groupBlock}</h5>
-        //               <h5 class='card-facts'>${hydrogen.facts}</h5>
-        //               <h5 class='card-yearDiscovered'>${hydrogen.yearDiscovered}</h5>
-        //             </div>
-        //           </div>
+        document.getElementById('calcium').innerHTML =
+            `<div class="modal-dialog modal-lg">
+            <div class="modal-content">
+            <div class='modal-footer'>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class='modal-body' style='margin: 2em;'>
+            <div>
+                    <h1 class='card-symbol'>${calcium.symbol}</h1>
+                    <h3 class='card-name'>${calcium.name}</h3>
+                    <h5 class='card-groupBlock'>${calcium.groupBlock}</h5>
+                    <h5 class='card-facts'>Facts: ${calcium.facts}</h5>
+                    <h5 class='card-yearDiscovered'>Year Discovered: ${calcium.yearDiscovered}</h5>
+                    <h5 class='card-history'>History: ${calcium.history}</h5>
+                    </div>
+                    </div>
+            </div>
+            </div>
+            </div>`
 
-        //         </div>
+        document.getElementById('nitrogen').innerHTML =
+            `<div class="modal-dialog modal-lg">
+            <div class="modal-content">
+            <div class='modal-footer'>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class='modal-body' style='margin: 2em;'>
+            <div>
+                    <h1 class='card-symbol'>${nitrogen.symbol}</h1>
+                    <h3 class='card-name'>${nitrogen.name}</h3>
+                    <h5 class='card-groupBlock'>${nitrogen.groupBlock}</h5>
+                    <h5 class='card-facts'>Facts: ${nitrogen.facts}</h5>
+                    <h5 class='card-yearDiscovered'>Year Discovered: ${nitrogen.yearDiscovered}</h5>
+                    <h5 class='card-history'>History: ${nitrogen.history}</h5>
+                    </div>
+                    </div>
+            </div>
+            </div>
+            </div>`
 
-        //       </div>
-        //     </div>
-        // </div>`
+        document.getElementById('phosphorus').innerHTML =
+            `<div class="modal-dialog modal-lg">
+            <div class="modal-content">
+            <div class='modal-footer'>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class='modal-body' style='margin: 2em;'>
+            <div>
+                    <h1 class='card-symbol'>${phosphorus.symbol}</h1>
+                    <h3 class='card-name'>${phosphorus.name}</h3>
+                    <h5 class='card-groupBlock'>${phosphorus.groupBlock}</h5>
+                    <h5 class='card-facts'>Facts: ${phosphorus.facts}</h5>
+                    <h5 class='card-yearDiscovered'>Year Discovered: ${phosphorus.yearDiscovered}</h5>
+                    <h5 class='card-history'>History: ${phosphorus.history}</h5>
+                    </div>
+                    </div>
+            </div>
+            </div>
+            </div>`
 
-
-
-
-
-
-        //     <h1 class='card-symbol' > ${ hydrogen.symbol }</h1 >
-        // <h3 class='card-name'>${hydrogen.name}</h3>
-        // <h5 class='card-groupBlock'>${hydrogen.groupBlock}</h5>
-        // <h5 class='card-facts'>Facts: ${hydrogen.facts}</h5>
-        // <h5 class='card-yearDiscovered'>Year Discovered: ${hydrogen.yearDiscovered}</h5>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // let carb = response.data[7];
-        // document.getElementById('carbon').innerHTML = `<div class="card" id="${carb.groupBlock}">
-        // <h1 id="card-symbol">${carb.symbol}</h1>
-        // <h3 id="card-name">${carb.name}</h3>
-        // <h5 id="card-groupBlock">${carb.groupBlock}</h5>
-        // <h5 id="facts">Facts: ${carb.facts}</h5>
-        // <h5 id="yearDiscovered">Year Discovered: ${carb.yearDiscovered}</h5>
-        // <h5 id="history">History: ${carb.history}</h5>
-        // </div>`
 
 
 
     });
-
-
-// ===== add accordion mode ===== 
-// $("#hydrogen").collapse('show');
-// changeBtnColor();
-// $("button[name='hydrogen']").addClass("btn btn-primary");
-
-// function changeBtnColor() {
-//     $(".infoCollapse").removeClass("btn btn-primary");
-//     $(".infoCollapse").addClass("btn btn-primary");
-// }
-
-// function toggleFunction(name) {
-//     $(".collapse").collapse('hide');
-//     changeBtnColor();
-//     setTimeout(function () { openSelected(name) }, 360);
-// }
-
-// function openSelected(name) {
-//     btnSelection = "button" + "[name=" + name + "]";
-//     $(btnSelection).addClass("btn btn-primary");
-//     selection = "#" + name;
-//     $(selection).collapse("toggle");
-// }
 
 
 // ===== Scroll to Top ==== 
@@ -135,14 +161,3 @@ $('#return-to-top').click(function () {      // When arrow is clicked
     }, 500);
 });
 
-
-
-// $('#exampleModalLabel').on('show.bs.modal', function (event) {
-//     var button = $(event.relatedTarget) // Button that triggered the modal
-//     var recipient = button.data('show') // Extract info from data-* attributes
-//     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-//     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-//     var modal = $(this)
-//     modal.find('.modal-title').text('New message to ' + recipient)
-//     modal.find('.modal-body input').val(recipient)
-// })
